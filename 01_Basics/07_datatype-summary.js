11e603a8fdfc1e925de31560c2c94ead3b77d1d2
Raj9536 <rajmourya9536@gmail.com>
2024-07-26 22:42:48 +0530

    data type-summary

---
+++ 01_Basics/07_datatype-summary.js
@@ -0,0 +1,32 @@
+// primitive
+
+//7 types : string, Number , Boolean , null , undefined, bigInt
+
+//Reference (Non primitive)
+
+// Array , object , functions
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+const bigNumber = 355456212115555n
+
+const heros = ["shaktiman" , "nagraaj" , "ironman"]
+let myobj={
+
+
+    name : "raj",
+    age : 22,
+}
+
+function myFunction() {
+    console.log("hello world");
+}
+
+console.log(typeof bigNumber);
+
+console.log(typeof myFunction);
+ console.log(typeof anotherId);
