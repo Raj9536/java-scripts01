d958665d17e12323fca68b257de3fb7c58996381
Raj9536 <rajmourya9536@gmail.com>
2024-07-31 21:48:21 +0530

    update

---
+++ 02_Basics/Array.js
@@ -0,0 +1,25 @@
+const myArr =  [0 ,1 ,2 ,3 ,4 ,5]
+// console.log(myArr[0]) // 0
+// console.log(myArr[4])
+// console.log(myArr[2])
+
+//const myHeros = ["IronMan", 'naaraj']
+// console.log(myHeros[0]) // IronMan
+ 
+//Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.push(8)
+// myArr.pop()
+
+myArr.unshift(9)
+myArr.shift()
+console.log(myArr.includes(8))
+console.log(myArr.indexOf(4));
+ // [0, 1, 2, 3, 4
+
+ const newArr = myArr.join()
+
+ slice // for create a section of any array
+ splice // it cut the selected partof the array and return manupulated array 
