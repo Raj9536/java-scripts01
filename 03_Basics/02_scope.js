56189ebd51d04de14717e86cd9712f620c9e32cc
Raj9536 <rajmourya9536@gmail.com>
2024-08-03 23:18:32 +0530

    iifi

---
+++ 03_Basics/02_scope.js
@@ -0,0 +1,52 @@
+
+
+if (true){
+    let a = 10
+const b = 20
+var c = 30
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username ="Raj"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    
+    two()
+}
+
+// one()
+
+if(true){
+    const username = "raj"
+    if(username=== "raj"){
+        const website ="youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+    
+}
+// console.log(username);
+
+
+// ===============================interts======
+
+function addon(num){
+    return num + 1
+}
+addon(5)
+
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(5)
