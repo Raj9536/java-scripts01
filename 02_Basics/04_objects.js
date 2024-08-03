4f14c7fbe09728a8ba516aea72e16f0bf5338a63
Raj9536 <rajmourya9536@gmail.com>
2024-08-03 18:12:30 +0530

    json

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -1,4 +1,4 @@
-// const tinderUser = new Object()
+const tinderUser = new Object()
 // const tinderuser{}
 
 tinderUser.id ="123abc"
@@ -16,3 +16,39 @@ const regularUser = {
         }
     }
 }
+
+// console.log(regularUser.fullName.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+// const obj3 = {obj1, obj2}
+// const obj3=Object.assign(obj1,obj2)
+
+const obj3 = {...obj1,...obj2}
+// console.log(obj3);
+
+const users = {
+    id: 1,
+    email: "h@gamil.com"
+}
+
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty("isLoggesIn"));
+
+const course ={
+    name: "javascript",
+    price: "999",
+    instructor:"raj"
+}
+
+// course.instructor
+
+const {instructor: instructor} =course
+// console.log(instructor);
+// console.log(course);
+
